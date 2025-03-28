1ba024228c485bd9214eecc1aa92220fd4ca1068
EBSiNoah <kyb0809tg@naver.com>
2025-03-28 12:23:24 +0900

    add plugin

---
+++ MyIDPlugin/Source/MyIDPlugin/Private/MyIDPluginBPLibrary.cpp
@@ -0,0 +1,26 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyIDPluginBPLibrary.h"
+#include "MyIDPlugin.h"
+
+UMyIDPluginBPLibrary::UMyIDPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UMyIDPluginBPLibrary::MyIDPluginSampleFunction(float Param)
+{
+	return -1;
+}
+
+void UMyIDPluginBPLibrary::SetUserName(const FString& NewName)
+{
+	UserName = NewName;
+	UE_LOG(LogTemp, Log, TEXT("UserName set to: %s"), *UserName);
+}
+
+FString UMyIDPluginBPLibrary::GetUserName()
+{
+	return UserName;
+}
